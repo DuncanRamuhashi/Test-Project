e44f59a725e392ad2993e89d6017d6754c2fff3f
Duncan Ramuhashi <137775255+DuncanRamuhashi@users.noreply.github.com>
2026-08-31 12:20:21 +0200

    with a person as an example

--- Test Project/Program.cs
+++ Test Project/Program.cs
@@ -1,24 +1,56 @@
-﻿// ARRAY
-string[] courts = { "Johannesburg", "Pretoria", "Soweto" };
+﻿using System;
+using System.Collections.Generic;
 
-Console.WriteLine(courts[0]);
+class Program
+{
+    static void Main()
+    {
+        // ARRAY
+        string[] courts = { "Johannesburg", "Pretoria", "Soweto" };
 
+        Console.WriteLine(courts[0]);
 
-// LIST
-List<string> cases = new List<string>();
+        // LIST
+        List<string> cases = new List<string>();
 
-cases.Add("State vs John");
-cases.Add("State vs James");
-cases.Add("State vs Peter");
+        cases.Add("State vs John");
+        cases.Add("State vs James");
+        cases.Add("State vs Peter");
 
-Console.WriteLine(cases[1]);
+        Console.WriteLine(cases[1]);
 
+        // DICTIONARY
+        Dictionary<int, string> caseNumbers = new Dictionary<int, string>();
 
-// DICTIONARY
-Dictionary<int, string> caseNumbers = new Dictionary<int, string>();
+        caseNumbers.Add(1, "JHB001");
+        caseNumbers.Add(2, "JHB002");
+        caseNumbers.Add(3, "JHB003");
 
-caseNumbers.Add(1, "JHB001");
-caseNumbers.Add(2, "JHB002");
-caseNumbers.Add(3, "JHB003");
+        Console.WriteLine(caseNumbers[2]);
 
-Console.WriteLine(caseNumbers[2]);
+        // DICTIONARY WITH OBJECTS
+
+        Dictionary<int, Person> people = new Dictionary<int, Person>();
+
+        people.Add(1, new Person
+        {
+            Name = "John",
+            Age = 25
+        });
+
+        people.Add(2, new Person
+        {
+            Name = "James",
+            Age = 30
+        });
+
+        Console.WriteLine(people[1].Name);
+        Console.WriteLine(people[1].Age);
+    }
+}
+
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
