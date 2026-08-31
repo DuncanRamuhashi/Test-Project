329181e0924b7abfaa05b206c624a67003b462a0
Duncan Ramuhashi <137775255+DuncanRamuhashi@users.noreply.github.com>
2026-08-31 10:46:20 +0200

    Add project files.

---
+++ Test Project/Program.cs
@@ -0,0 +1,24 @@
+﻿// ARRAY
+string[] fruits = { "Apple", "Banana", "Orange" };
+
+Console.WriteLine(fruits[0]);
+
+
+// LIST
+List<string> cars = new List<string>();
+
+cars.Add("BMW");
+cars.Add("Toyota");
+cars.Add("Ford");
+
+Console.WriteLine(cars[1]);
+
+
+// DICTIONARY
+Dictionary<int, string> students = new Dictionary<int, string>();
+
+students.Add(1, "John");
+students.Add(2, "James");
+students.Add(3, "Peter");
+
+Console.WriteLine(students[2]);
